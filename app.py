02de8a0faaef763970ba5f2ae3062d68158a4ce0
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2021-01-14 00:59:56 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from flask import Flask,render_template,request,flash
 import pickle
 import os
 
 app=Flask(__name__)
-app.config["SECRET_KEY"]=os.environ.get["SECRET_KEY"]
+app.config["SECRET_KEY"]=os.environ.get("SECRET_KEY")
 Model = pickle.load(open('model.pkl','rb'))
 
-@app.route("/",methods=["POST","GET"])	
+@app.route("/",methods=["POST","GET"])
 @app.route("/predict",methods=['POST','GET'])
 def predict():
 	if request.method=="POST":
@@ -23,6 +25,6 @@ def predict():
 		output=round(prediction,-3)
 		flash(f"The price of the car is {output}","success")
 	return render_template("index.html")
-	
+
 if __name__=="__main__":
 	app.run()
